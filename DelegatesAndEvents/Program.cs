eda15500c63aa355fa40edf7d9ef0682d7366a52
Humoyun Ochilov Ravshan o'g'li <43071439+dotNet-developer1995@users.noreply.github.com>
2024-02-15 12:01:47 +0500

    Add project files.

---
+++ DelegatesAndEvents/Program.cs
@@ -0,0 +1,28 @@
+﻿using DelegatesAndEvents;
+
+public class Program
+{
+
+    public static void Tugma_Click()
+    {
+        Console.WriteLine("Tugma bosildi.");
+    }
+    public static void Button_Click()
+    {
+        Console.WriteLine("Button clicked.");
+    }
+
+    public static void Main(string[] args)
+    {
+        Tugma tugma = new Tugma();
+        tugma.Click += Tugma_Click;
+        tugma.Click += Button_Click;
+
+        tugma.Simulation();
+
+        tugma.Click -= Tugma_Click;
+        tugma.Click -= Button_Click;
+
+        Console.ReadLine();
+    }
+}
---
+++ DelegatesAndEvents/Tugma.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEvents
+{
+    public delegate void TugmaDelegati();
+    public class Tugma
+    {
+        public event TugmaDelegati Click;
+
+        public void Simulation()
+        {
+            if (Click != null) 
+            {
+                Click();
+            }
+        }
+    }
+}
